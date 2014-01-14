115ccd0a70e22dc4885afebae51a9c88db1f7aed
Tim Plunkett <git@plnktt.com>
2014-01-13 16:57:02 -0800

    Add JS for slider buttons.

--- js/gsb_az_filter.js
+++ js/gsb_az_filter.js
@@ -1,11 +1,11 @@
-(function ($, window) {
+(function ($) {
 
 "use strict";
 
 /**
  * Transform a set of A-Z links to update via AJAX.
  */
-Drupal.behaviors.gsbAzFilter = {
+Drupal.behaviors.gsbAzFilterLinks = {
   attach: function (context, settings) {
     // Find the input element that needs to be updated.
     var $input = $("[name='" + settings.gsb_az_filter.form_item_name + "']");
@@ -33,4 +33,59 @@ Drupal.behaviors.gsbAzFilter = {
   }
 };
 
-})(jQuery, window);
+/**
+ * Transform a set of A-Z links to update via AJAX.
+ */
+Drupal.behaviors.gsbAzFilterSlider = {
+  attach: function () {
+    var $wrapper = $('#edit-gsb-az-filter-last-name-wrapper');
+    var $slider = $wrapper.find('ul');
+    var $prev = $wrapper.find('.prev');
+    var $next = $wrapper.find('.next');
+
+    // The single width of a slider item.
+    var single_width = $slider.find('li').eq(0).width();
+    // How many items will slide by per click
+    var items_per_click = Math.floor($slider.width() / single_width);
+    // The maximum number of times this can be clicked.
+    var max_click = Math.floor($slider.children().length / items_per_click);
+
+    // Track the number of clicks.
+    var click = 0;
+
+    $prev.click(function () {
+      buttonClick(this, -1);
+    });
+
+    $next.click(function () {
+      buttonClick(this, 1);
+    });
+
+    function buttonClick(el, directional_multiplier) {
+      // If the button is disabled, prevent clicking.
+      if (!$(el).hasClass('enabled') || $slider.is(':animated')) {
+        return;
+      }
+
+      // Determine how far to adjust the slider.
+      // current offset - (width of visible items * direction)
+      var offset = parseInt($slider.css('marginLeft'), 10) - (items_per_click * single_width * directional_multiplier);
+      $slider.animate({'margin-left': offset}, 500, 'swing');
+
+      click += directional_multiplier;
+
+      if (click == 0) {
+        $prev.removeClass('enabled');
+      }
+      else if (click == max_click) {
+        $next.removeClass('enabled');
+      }
+      else {
+        $prev.addClass('enabled');
+        $next.addClass('enabled');
+      }
+    }
+  }
+};
+
+})(jQuery);
